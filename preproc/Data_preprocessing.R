9c6b5646a2f63792c40537ff81bd0ed4acda0cb1
Martin Vasilev <mvasilev@bournemouth.ac.uk>
2017-08-18 21:40:19 +0100

    more pre-processing

--- preproc/Data_preprocessing.R
+++ preproc/Data_preprocessing.R
@@ -10,6 +10,7 @@ sound_check<- soundCheck()
 # further development:
 # was boundary crossed in a saccade?
 # how many ms before next fix started?
+# CHANGE DEFINITION OF HOOK!: crossing is not a hook if next fix is within 1 ppl 
 
 
 save(sound_check, file= "preproc/sound_check.Rda")
@@ -33,19 +34,11 @@ infix<- which(sound_check$nextFlag=="EFIX" & sound_check$delFix>5)
 infixn<- length(infix)
 sound_check<- sound_check[-infix,]
 
-cat(sprintf("%d percent of data excluded due to blinks", (nblinks/nobs)*100))
-cat(sprintf("%d percent of data excluded due to in-fixations", (infixn/nobs)*100))
-cat(sprintf("%d percent of data remains for analysis", (nrow(sound_check)/nobs)*100))
+cat(sprintf("%f percent of data excluded due to blinks", (nblinks/nobs)*100))
+cat(sprintf("%f percent of data excluded due to in-fixations", (infixn/nobs)*100))
+cat(sprintf("%f percent of data remains for analysis", (nrow(sound_check)/nobs)*100))
 
 sound_check<- subset(sound_check, delFix<80)
-
-
-#######
-mean(sound_check$delFix)
-
-hist(sound_check$delFix, xlab= "Time until SFIX flag (in ms)", main= "Sound implementation timing")
-abline(v = mean(sound_check$delFix), col= "darkred", lwd=3)
-
 
 
 ###############################
@@ -53,7 +46,15 @@ abline(v = mean(sound_check$delFix), col= "darkred", lwd=3)
 ###############################
 
 source("functions/paraFix.R")
+source("functions/assign_cond.R")
 
-raw_fix<- paraFix(plot=F)
+raw_fix<- paraFix(plot=T)
+
+raw_fix<- assign_cond(sound_check, raw_fix)
+
+#110
+
+save(raw_fix, file= "data/raw_fix.Rda")
+
 
 
