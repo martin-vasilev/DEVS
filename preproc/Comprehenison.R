30b727cc74ee4773ffd644618e8aa1e5759000c3
Martin Vasilev <mvasilev@bournemouth.ac.uk>
2017-10-24 13:42:07 +0100

    fixed git problems

---
+++ preproc/Comprehenison.R
@@ -0,0 +1,12 @@
+library(readr)
+q <- read_delim("preproc/raw_data/q.txt", "\t", escape_double = FALSE, trim_ws = TRUE)
+
+library(reshape)
+
+DesComp<- melt(q, id=c('subject', 'item', 'questcond'), 
+             measure=c("accuracy"), na.rm=TRUE)
+mQ<- cast(DesComp, subject ~ variable
+           ,function(x) c(M=signif(mean(x),3)
+                          , SD= sd(x) ))
+
+save(q, file= "data/q.Rda")
