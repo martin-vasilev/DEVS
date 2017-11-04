48e4a11db34cbc50e87f48ab6f6182c97feeb829
Martin Vasilev <mvasilev@bournemouth.ac.uk>
2017-11-04 20:52:42 +0000

    clean-up

--- Test.R
+++
@@ -1,6 +0,0 @@
-
-rm(list= ls())
-
-source("functions/paraFix.R")
-
-raw_fix<- paraFix(plot = T, data_list ="C:/Users/mvasilev/Documents/files.txt", ResX= 1920, ResY=1080, maxtrial = 51, align=FALSE)
